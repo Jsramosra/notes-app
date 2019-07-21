f4239d9271a532136a087fa9733dab01f7b5be96
Juan Sebastian Ramos Ramírez <jsramos@unbosque.edu.co>
2019-07-21 11:12:30 -0500

    Añadido captcha reCaptcha v2 página de logueo

--- src/index.js
+++ src/index.js
@@ -5,10 +5,14 @@ const mor = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require('passport');
+const bodyParser = require('body-parser');
+
+
 //Inicializanciones
 const app = express();
 require('./database');
 require('./config/passport');
+
 //Settings
 app.set('port', 3000);
 app.set('views', path.join(__dirname, 'views'));
@@ -19,9 +23,13 @@ app.engine('.hbs', exhbs({
     partialsDir: path.join(app.get('views'), 'partials'),
     extname: '.hbs'
 }));
+
 app.set('view engine', '.hbs');
+
 //MiddleWares
 app.use(express.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use(mor('_method'));
 app.use(session({
     secret: 'wolfskull2019!',
@@ -32,6 +40,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
+
 //Global Variables
 app.use((req,res,next) =>{
     res.locals.success_msg = req.flash('success_msg');
@@ -40,6 +49,7 @@ app.use((req,res,next) =>{
     res.locals.user = req.user || null;
     next();
 });
+
 //Routes
 app.use(require('./routes/index'));
 app.use(require('./routes/notes'));
@@ -47,6 +57,7 @@ app.use(require('./routes/users'));
 
 //Static Files
 app.use(express.static(path.join(__dirname,'public')));
+
 //Server Listening
 app.listen(app.get('port'), () => {
     console.log('server on port: ', app.get('port'));
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,15 +2,42 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const passport = require('passport');
+const request = require('request');
+
 router.get('/users/signin', (req, res) =>{
     res.render('users/signin');
 });
 
-router.post('/users/signin', passport.authenticate('local', {
-    successRedirect: '/notes',
-    failureRedirect: '/users/signin',
-    failureFlash: true
-}));
+router.post('/users/signin', (req,res) => {
+    const token = req.body['g-recaptcha-response'];
+
+    if(token == ''){
+        req.flash('error_msg', 'Verify captcha');
+        res.redirect('/users/signin');
+        
+
+    }else{
+
+    const secretKey = "6Ld1lq4UAAAAAE2HdoPYOFZN-uj4ATpbStqrm0DJ";
+    const verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + token + "&remoteip=" + req.connection.remoteAddress;
+    request(verificationUrl,function(error,response,body) {
+        console.log(body);
+        body = JSON.parse(body);
+       if(body.success !== undefined && !body.success) {
+          req.flash('error_msg', 'Ploblem with captcha verification, try again');
+          res.redirect('/users/signin');
+        }
+   
+      });
+    
+    passport.authenticate('local', {
+        successRedirect: '/notes',
+        failureRedirect: '/users/signin',
+        failureFlash: true
+    })(req,res);
+}
+});
+
 
 router.get('/users/signup', (req, res) =>{
     res.render('users/signup');
@@ -39,13 +66,16 @@ router.post('/users/signup', async (req,res) =>{
         errors.push({text: 'Password must be at least 4 characters'});
     }
 
-    if(errors.length>0){
+    if(errors.length>0)
+    {
         res.render('users/signup', {errors, name, email, password, confirm_password});
-    }else{
+    }
+    else
+    {
         const emailUser = await User.findOne({email: email});
         if(emailUser){
             req.flash('error_msg', 'The email is already on use');
-            req.redirect('/users/signup');
+            res.redirect('/users/signup');
         }
         const newUser = new User({name, email, password});
          newUser.password = await newUser.encryptPassword(password);
